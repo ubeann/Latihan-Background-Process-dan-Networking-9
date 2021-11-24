48f67aaa08f08d82ca5fdf14bb7cc294133ed65b
Ubean <ubeannn@gmail.com>
2021-11-24 12:29:03 +0700

    GET data using Retrofit

---
+++ app/src/main/java/com/dicoding/latihan_background_process_dan_networking_9/ApiService.kt
@@ -0,0 +1,11 @@
+package com.dicoding.latihan_background_process_dan_networking_9
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
